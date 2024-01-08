d9fb97202ec43bfebdad28e53f57ce6a2992bd4a
IAMOTAS <niiotasalokome@gmail.com>
2024-01-08 16:58:59 +0000

    typography

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,28 +1,28 @@
+import React from 'react'; // Import React
 import './App.css';
-import{Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import Home from './pages/Home';
 import CountryDetails from './pages/CountryDetails';
+import { lightTheme, darkTheme } from './components/Theme';
+import { useState } from 'react';
 import Header from './components/Header';
 
+const App: React.FC = () => {
+  const [themeSwitch, setThemeSwitch] = useState(true);
 
-
-const App = () => {
-  return 
+  return (
     <>
-      
-      <Header/>
-
-      <Routes>
-        <Route path='/' element =
-        {<Home />} /> 
-
-        <Route path='/countrydetails/:id' element =
-        {<CountryDetails />} />   
-      </Routes>
-
-    </>;
-  
+      <ThemeProvider theme={themeSwitch ? lightTheme : darkTheme}>
+        <CssBaseline />
+        <Header themeSwitch={themeSwitch} setThemeSwitch={setThemeSwitch} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/countrydetails/:id' element={<CountryDetails />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
-
+export default App;
--- typescript/src/components/Header.tsx
+++ typescript/src/components/Header.tsx
@@ -1,11 +1,24 @@
 
 
-const Header = () => {
+
+import { Paper, Stack, Typography } from "@mui/material";
+
+
+type HeaderProps = {
+    themeSwitch: boolean;
+    setThemeSwitch: (value: boolean) => void; // Assuming setThemeSwitch is a function that takes a boolean
+};
+
+const Header: React.FC<HeaderProps> = ({}) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Paper elevation={1}>
+      <Stack flexDirection={'row'} justifyContent={'space-around'} alignItems={'center'} py={1.5}>
+        <Typography fontFamily={'Nunito Sans'} fontWeight={800} sx={{ fontSize: { lg: '1.8rem', md: '1.5rem', sm: '1rem', xs: '0.09rem' } }}>
+          where in the world?
+        </Typography>
+      </Stack>
+    </Paper>
+  );
 }
 
-export default Header
+export default Header;
